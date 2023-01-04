c0e440c1cf946eae1394e7f1281b4cc32aeb85ba
Tim Schwenke <tim.schwenke@trallnag.com>
2023-01-04 20:29:11 +0100

    test: Remove now redundant testcases and testdata

--- config_test.go
+++ config_test.go
@@ -35,46 +35,6 @@ func TestNewConfig(t *testing.T) {
 		ErrSubstr:  "seconds smaller",
 		expSuccess: false,
 	}, {
-		name:       "5_metric_namespace_empty",
-		configFile: "config-metric-namespace-empty.yaml",
-		ErrSubstr:  "missing config: metric.namespace",
-		expSuccess: false,
-	}, {
-		name:       "6_metric_name_empty",
-		configFile: "config-metric-name-empty.yaml",
-		ErrSubstr:  "missing config: metric.name",
-		expSuccess: false,
-	}, {
-		name:       "7_dimension_name_empty",
-		configFile: "config-dimension-name-empty.yaml",
-		ErrSubstr:  "missing config: metric.dimensions",
-		expSuccess: false,
-	}, {
-		name:       "8_dimension_value_empty",
-		configFile: "config-dimension-value-empty.yaml",
-		ErrSubstr:  "missing config: metric.dimensions",
-		expSuccess: false,
-	}, {
-		name:       "9_no_targets",
-		configFile: "config-no-targets.yaml",
-		ErrSubstr:  "missing config: targets",
-		expSuccess: false,
-	}, {
-		name:       "10_kind_not_supported",
-		configFile: "config-unsupported-kind.yaml",
-		ErrSubstr:  "kind not supported",
-		expSuccess: false,
-	}, {
-		name:       "11_mode_not_supported",
-		configFile: "config-unsupported-mode.yaml",
-		ErrSubstr:  "mode not supported",
-		expSuccess: false,
-	}, {
-		name:       "12_target_kind_empty",
-		configFile: "config-target-kind-empty.yaml",
-		ErrSubstr:  "missing config: target[2].kind",
-		expSuccess: false,
-	}, {
 		name:       "13_invalid_log_level",
 		configFile: "config-invalid-log-level.yaml",
 		ErrSubstr:  "logging.level not supported",
@@ -83,21 +43,6 @@ func TestNewConfig(t *testing.T) {
 		name:       "14_log_level_default",
 		configFile: "config-log-level-default.yaml",
 		expSuccess: true,
-	}, {
-		name:       "15_target_no_name",
-		configFile: "config-target-no-name.yaml",
-		ErrSubstr:  "missing config: target[1].name",
-		expSuccess: false,
-	}, {
-		name:       "16_target_no_mode",
-		configFile: "config-target-no-mode.yaml",
-		ErrSubstr:  "missing config: target[1].mode",
-		expSuccess: false,
-	}, {
-		name:       "17_target_no_namespace",
-		configFile: "config-target-no-namespace.yaml",
-		ErrSubstr:  "missing config: target[1].namespace",
-		expSuccess: false,
 	}} {
 		t.Run(tc.name, func(t *testing.T) {
 			c, err := NewConfig(filepath.Join("testdata", tc.configFile))
